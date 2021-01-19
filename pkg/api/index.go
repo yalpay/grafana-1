3c761fe1bdd01a46020a0f5a39eddb362df83867
yalpay <yalpay38@gmail.com>
2021-01-19 11:21:54 +0000

    playlists and snapshots sections are removed

--- pkg/api/index.go
+++ pkg/api/index.go
@@ -163,16 +163,6 @@ func (hs *HTTPServer) getNavTree(c *models.ReqContext, hasEditPerm bool) ([]*dto
 		{Text: "Home", Id: "home", Url: setting.AppSubUrl + "/", Icon: "home-alt", HideFromTabs: true},
 		{Text: "Divider", Divider: true, Id: "divider", HideFromTabs: true},
 		{Text: "Manage", Id: "manage-dashboards", Url: setting.AppSubUrl + "/dashboards", Icon: "sitemap"},
-		{Text: "Playlists", Id: "playlists", Url: setting.AppSubUrl + "/playlists", Icon: "presentation-play"},
-	}
-
-	if c.IsSignedIn {
-		dashboardChildNavs = append(dashboardChildNavs, &dtos.NavLink{
-			Text: "Snapshots",
-			Id:   "snapshots",
-			Url:  setting.AppSubUrl + "/dashboard/snapshots",
-			Icon: "camera",
-		})
 	}
 
 	navTree = append(navTree, &dtos.NavLink{
